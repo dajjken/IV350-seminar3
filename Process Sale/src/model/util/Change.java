50f200c2fece17450bbc4a21e73b812e1adf693c
dajjken <d.forsling@gmail.com>
2020-04-27 21:37:35 +0200

    Added comments

--- Process Sale/src/model/util/Change.java
+++ Process Sale/src/model/util/Change.java
@@ -2,18 +2,27 @@ package model.util;
 
 import model.DTO.SaleInformation;
 
+/**
+ * Represents the change that the customer will receive after cash is paid.
+ *
+ */
 public class Change {
 	
 	private Amount amount;
 	
+	/**
+	 * Creates an instance using the <code>TotalPrice</code> in <code>SaleInformation</code>.
+	 *  
+	 * @param saleInfo Contains information about the sale.
+	 */
 	public Change(SaleInformation saleInfo)
 	{
 		this.amount = saleInfo.getTotalPrice().addPriceAndVAT();
 	}
 
 	/**
-	 *  Updates the change 
-	 * @param paidAmount
+	 *  Updates the change by subtracting paidAmount with the total price.
+	 * @param paidAmount Represent the <code>Amount</code> paid in cash.
 	 */
 	public void calculateChange(Amount paidAmount) 
 	{
@@ -21,6 +30,10 @@ public class Change {
 	
 	}
 	
+	/**
+	 * Returns the <code>Amount</code>-instance of <code>Change</code>.
+	 * @return Returns the change.
+	 */
 	public Amount getChange() {
 		return amount;
 	}
