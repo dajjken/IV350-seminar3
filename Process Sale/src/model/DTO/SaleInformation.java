b1ea23044452dec5d3bba07f4d7b9a76e646391a
dajjken <d.forsling@gmail.com>
2020-04-28 12:52:39 +0200

    Added some comments

--- Process Sale/src/model/DTO/SaleInformation.java
+++ Process Sale/src/model/DTO/SaleInformation.java
@@ -4,24 +4,33 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 
-import integration.ItemDescription;
 import model.POS.Item;
 import model.POS.Sale;
 import model.util.Amount;
 import model.util.TotalPrice;
 
+/**
+ * This class is used as an DTO that contains information about the completed <code>Sale</code>.
+ * When the sale is completed, a first instance of this class is created. The first creation 
+ * contains all information of the sale, except for the amount paid by the customer, and the
+ * change that will be handed out. After the customer have paid in cash, the final 
+ * <code>SaleInformation</code> is created with all important information complete. 
+ */
 public class SaleInformation {
 	
-	private ArrayList <Item> itemList = new ArrayList<>();
-	private ItemDescription mostRecentScannedItem;
-	LocalTime saleTime;
-	LocalDate date;
-	TotalPrice totalPrice;
-	Amount totalVAT;
-	Amount amountPaid = new Amount(0);
-	Amount change = new Amount(0);
-	String itemListAsString;
+	private final ArrayList <Item> itemList;
+	private final LocalTime saleTime;
+	private final LocalDate date;
+	private final TotalPrice totalPrice;
+	private final Amount totalVAT;
+	private final Amount amountPaid;
+	private final Amount change;
+	private final String itemListAsString;
 	
+	/**
+	 * Creates the first instance used by the application.
+	 * @param sale Contains all information of the <code>Sale</code>.
+	 */
 	public SaleInformation(Sale sale)
 	{
 		this.itemList = sale.getItemsInPurchase();
@@ -29,9 +38,20 @@ public class SaleInformation {
 		this.date = LocalDate.now();
 		this.totalPrice = sale.getTotalPrice();
 		this.totalVAT = totalPrice.getTotalVAT();
+		this.amountPaid = new Amount(0);
+		this.change = new Amount(0);
 		this.itemListAsString = sale.itemsToString(); 
 	}
 	
+	/**
+	 * Creates the final instance by copying the previous instance, and adding new values 
+	 * to change and amountPaid.
+	 * 
+	 * @param saleInfo The first created instance of <code>SaleInformation</code>.
+	 * @param change Represents how much change that will be handed out, as an <code>Amount</code>-object.
+	 * @param amountPaid Represents how much cash the customer paid.
+	 * 
+	 */
 	public SaleInformation(SaleInformation saleInfo, Amount change, Amount amountPaid)
 	{
 		this.itemList = saleInfo.itemList;
@@ -45,54 +65,57 @@ public class SaleInformation {
 	}
 	
 	/**
-	 * Temporary printmethod
+	 * Gets the <code>TotalPrice</code> field.
+	 * @return the Total price.
 	 */
-	
-	public void printList() 
-	{	
-		for(Item item: itemList)
-		{
-			System.out.println(item.toString());
-		}	
-		System.out.println("Total price:" + getTotalPrice().toString());
-	}
-	
-	/**
-	 * move to printer later
-	 */
-	public String toString() 
-	{
-		return "";
-	}
-
-	public Amount getTotalPriceAsAmount() {
-		return totalPrice.getTotalPrice();
-	}
-	
 	public TotalPrice getTotalPrice() {
 		return totalPrice;
 	}
 
+	/**
+	 * Gets the <code>LocalTime</code> saleTime field.
+	 * @return time of the sale.
+	 */
 	public LocalTime getSaleTime() {
 		return saleTime;
 	}
 
+	/**
+	 * Gets the <code>LocalDate</code> date field.
+	 * @return  the date of the sale.
+	 */
 	public LocalDate getDate() {
 		return date;
 	}
 
+	/**
+	 * Gets the <code>Amount</code> totalVAT field.
+	 * @return totalVAT.
+	 */
 	public Amount getTotalVAT() {
 		return totalVAT;
 	}
 
+	/**
+	 * Gets the String-representation of the <code>ArrayList</code> of items.
+	 * @return itemListAsString.
+	 */
 	public String getItemListAsString() {
 		return itemListAsString;
 	}
 	
+	/**
+	 * Gets the <code>Amount</code> change field.
+	 * @return the change.
+	 */
 	public Amount getChange() {
 		return this.change;
 	}
 	
+	/**
+	 *  Gets the <code>Amount</code> amountPaid field.
+	 * @return amountPaid.
+	 */
 	public Amount getAmountPaid() {
 		return this.amountPaid;
 	}
