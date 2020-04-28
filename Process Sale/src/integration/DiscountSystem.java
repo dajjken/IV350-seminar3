278582b9c3ee0ac9a856a0353ff425170d914e87
dajjken <d.forsling@gmail.com>
2020-04-28 21:52:50 +0200

    Added comments

--- Process Sale/src/integration/DiscountSystem.java
+++ Process Sale/src/integration/DiscountSystem.java
@@ -3,33 +3,31 @@ package integration;
 import model.DTO.SaleInformation;
 import model.util.TotalPrice;
 
+
+/**
+ * Represents an external discount-database where all the discount rules is stored. 
+ * Not implemented in this class. 
+ *
+ */
 public class DiscountSystem {
 	
 	TotalPrice newTotalPrice;
 	
+	/**
+	 * Creates a new instance.
+	 */
 	public DiscountSystem() {
-		//System.out.println("Discount created");
-	}
-
-	public DiscountSystem getDiscountSystem() {
-		return this;
 	}
 
 	/**
-	 * Checks if the customer is egible for discount, since discount is not 
-	 * applied to this task, will always return the same totalprice.
-	 * @param customerID Represent the unique id for a customer
-	 * @param saleInfo Contains all sale information
-	 * @return The same totalprice 
+	 * Checks if the customer is eligible for discount. Since discount is not 
+	 * applied to this task, the same {@link TotalPrice} will always be returned.
+	 * @param customerID Represent the unique id for a customer.
+	 * @param saleInfo Contains sale information as a {@link SaleInformation}-object.
+	 * @return The same total price. 
 	 */
 	public TotalPrice checkDiscount(int customerID, SaleInformation saleInfo) {
-		if(true)
-		{
-			System.out.println("Not egible for discount");
-			return saleInfo.getTotalPrice();
-		}
-		else
-			return newTotalPrice;
+		return saleInfo.getTotalPrice();
 	}
 	
 }
