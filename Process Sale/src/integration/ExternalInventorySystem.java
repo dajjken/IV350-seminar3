f04029e86e692636cf59e42b1837371933a2de39
dajjken <d.forsling@gmail.com>
2020-04-28 11:22:08 +0200

    Changed the price of some items, and added some comments.

--- Process Sale/src/integration/ExternalInventorySystem.java
+++ Process Sale/src/integration/ExternalInventorySystem.java
@@ -5,26 +5,30 @@ import java.util.ArrayList;
 import model.DTO.SaleInformation;
 import model.util.Amount;
 
+/**
+ * Represents a inventory at a retail store. Since no real database is used, a few hardcoded 
+ * items is added to the created instance of this class.  
+ *
+ */
 public class ExternalInventorySystem {
 	
-//	private ExternalInventorySystem inventory;
 	private ArrayList<ItemDescription> goodsInStock = new ArrayList<ItemDescription>();
 
+	/**
+	 * Create an instance. Adds a few hardcoded number of <code>ItemDescription</code> to the 
+	 * <code>ArrayList</code> in this instance.
+	 */
 	public ExternalInventorySystem() {
 		
 		addGoods();
-		//System.out.println("Inventory created");	
 	}
-/*
-	public ExternalInventorySystem getExternalInventorySystem()
-	{
-		return this;
-	}
-*/	
+
 	
 	/**
-	 * Searches the database for a certain item.
-	 * @param itemID Represents the item searched for.
+	 * Searches the database for a certain <code>ItemDescription</code>. The itemID is compared
+	 * and the description is returned if found, if not, <code>null</code> is returned.
+	 * 
+	 * @param itemID Represents the ID of the item searched for.
 	 * @return the description of the found item. Returns null if item is not found.
 	 */
 	public ItemDescription findItem(int itemID)
@@ -41,27 +45,24 @@ public class ExternalInventorySystem {
 		}	
 		return null;
 	}
-	
-	private void printList() {
-		for(ItemDescription item: goodsInStock)
-		{
-			System.out.println(item);
-		}
-	}
+
 	/**
-	 * Updates the inventory after sale is completed, not implemented
+	 * Updates the inventory after sale is completed, not implemented.
 	 */
 	public void updateInventory(SaleInformation saleInfo) {
 		//some code
 	}
 	
+	/**
+	 * The hardcoded inventory.
+	 */
 	private void addGoods() {
 		goodsInStock.add(new ItemDescription(new Amount(50), "Butter", 0.06, 1010));
 		goodsInStock.add(new ItemDescription(new Amount(25), "Bread", 0.12, 2020));
-		goodsInStock.add(new ItemDescription(new Amount(10), "Flour", 0.12, 3030));
-		goodsInStock.add(new ItemDescription(new Amount(20), "Pasta", 0.12, 4040));
-		goodsInStock.add(new ItemDescription(new Amount(35), "Coffee", 0.25, 5050));
-		goodsInStock.add(new ItemDescription(new Amount(85), "Cheese", 0.12, 6060));
+		goodsInStock.add(new ItemDescription(new Amount(12), "Flour", 0.25, 3030));
+		goodsInStock.add(new ItemDescription(new Amount(16), "Pasta", 0.25, 4040));
+		goodsInStock.add(new ItemDescription(new Amount(40), "Coffee", 0.25, 5050));
+		goodsInStock.add(new ItemDescription(new Amount(100), "Cheese", 0.12, 6060));
 	
 	}
 
