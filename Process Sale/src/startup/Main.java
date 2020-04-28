0452fb36f9fb927a8a95e0116a36d2b38de395a8
dajjken <d.forsling@gmail.com>
2020-04-28 20:50:22 +0200

    Added some comments

--- Process Sale/src/startup/Main.java
+++ Process Sale/src/startup/Main.java
@@ -4,9 +4,16 @@ import controller.Controller;
 import integration.CreateSystems;
 import integration.Printer;
 import view.View;
-
+/**
+ *	Starts the entire application. 
+ */
 public class Main {
 
+	/**
+	 * The main method used to start the application. 
+	 * 
+	 * @param args The application does not take any command line parameters.
+	 */
 	public static void main(String[] args) {
 
 		CreateSystems system = new CreateSystems();
@@ -14,11 +21,7 @@ public class Main {
 		Controller controller = new Controller(system, printer);
 		View view = new View(controller);
 		
-		//view.hardcodedTestRun();
 		view.userTestRun();
-		
-		
-		
 	}
 
 }
