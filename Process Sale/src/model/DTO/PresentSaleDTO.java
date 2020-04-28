014a971092cbf60a65d8454db4cfdec347228e10
dajjken <d.forsling@gmail.com>
2020-04-28 10:36:33 +0200

    Set field as private and added some comments.

--- Process Sale/src/model/DTO/PresentSaleDTO.java
+++ Process Sale/src/model/DTO/PresentSaleDTO.java
@@ -3,24 +3,36 @@ package model.DTO;
 import model.POS.Item;
 import model.util.TotalPrice;
 
+/**
+ * This class is used as an DTO, and displays some information about the sale. 
+ *
+ */
 public class PresentSaleDTO {
 
 	private Item mostRecentScannedItem;
-	TotalPrice totalPrice;
+	private TotalPrice totalPrice;
 
+	/**
+	 * Creates an instance
+	 * @param scannedItem Represent the most recent scanned <code>Item</code>.
+	 * @param totalPrice The current <code>TotalPrice</code> of the sale.
+	 */
 	public PresentSaleDTO(Item scannedItem, TotalPrice totalPrice) {
 		this.mostRecentScannedItem = scannedItem;
 		this.totalPrice = totalPrice;
 		
 	}
 	
+	/**
+	 * Returns a String-representation of this instance. 
+	 */
 	public String toString()
 	{
 		if(mostRecentScannedItem.getQuantity()>1) {
 		return mostRecentScannedItem.getItemDescription().getName() + "\t\t"
 		+mostRecentScannedItem.getQuantity()+"*\t"+ mostRecentScannedItem.getItemDescription().getPrice()
 			+"\nVAT for this item: " + mostRecentScannedItem.getItemDescription().getVATCost()		
-			+"\nTotal Price: \t\t" + totalPrice.addPriceAndVAT()+"\n";
+			+"*"+mostRecentScannedItem.getQuantity()+"\nTotal Price: \t\t" + totalPrice.addPriceAndVAT()+"\n";
 		}
 		else
 			return mostRecentScannedItem.getItemDescription().getName() + "\t\t\t"
