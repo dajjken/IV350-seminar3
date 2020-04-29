a294f660cc2e6a52b1bcde2d7111b69207e73469
dajjken <d.forsling@gmail.com>
2020-04-29 10:35:03 +0200

    Added some comments.

--- Process Sale/src/model/POS/CashRegister.java
+++ Process Sale/src/model/POS/CashRegister.java
@@ -1,17 +1,24 @@
 package model.POS;
 
 import model.util.Amount;
-
+/**
+ * Represents a cash-register in a POS.
+ *
+ */
 public class CashRegister {
 	
-	Amount storedAmount;
+	private Amount storedAmount;
 	
+	/**
+	 * Creates a new instance with the {@link Amount} of 5000.
+	 */
 	public CashRegister() {
 		storedAmount= new Amount(5000);
 	}
 
 	/**
-	 * Updates the amount of cash stored in the register.
+	 * Updates the <code>Amount</code> of cash stored in the register.
+	 * 
 	 * @param cash represents the amount to be added
 	 */
 	public void updateAmountStored(Amount cash) {
